b322bc83819dc66e8e825c6dc9635cad7c8f1716
LexStorozhuk <sasha0878@yahoo.com>
2022-12-19 21:38:51 +0200

    Site for flowers shop

---
+++ js.js
@@ -0,0 +1,8 @@
+function popOpen() {
+    let  one = document.querySelector(".pop");
+     one.style.display = "block";
+}
+ function closePop() {
+    let  one = document.querySelector(".pop");
+    one.style.display = "none"; 
+ }
