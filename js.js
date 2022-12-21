a0bf6306527d24038458665aa6db6a343776bd03
LexStorozhuk <sasha0878@yahoo.com>
2022-12-21 23:06:35 +0200

    Making paralax

--- js.js
+++ js.js
@@ -6,3 +6,9 @@ function popOpen() {
     let  one = document.querySelector(".pop");
     one.style.display = "none"; 
  }
+ function openMain () {
+   let one = document.querySelector(".main");
+   two = document.querySelector(".intro")
+   one.style.display = "block";
+   two.style.display ="none"
+ }
