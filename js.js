4a4d9b180cf6dafc51c221d6baf70c59bf0e6edd
LexStorozhuk <sasha0878@yahoo.com>
2022-12-28 17:18:15 +0200

    New design

--- js.js
+++ js.js
@@ -12,3 +12,19 @@ function popOpen() {
    one.style.display = "block";
    two.style.display ="none"
  }
+ let map = 0;
+ function openLocation() {
+  map++;
+  let two= document.querySelector(".map_pop")
+  three = document.querySelector(".map_pop");
+
+  two.style.display="block"
+  three.style.animation="right_to_left 3s"
+
+if (map > 1) {
+ let three = document.querySelector(".map_pop");
+
+  three.style.animation="left_to_right 3s forwards  "
+  map=0;
+}
+ }
